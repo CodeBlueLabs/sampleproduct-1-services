999f691ddde5785c39bb6fd3c752896f3dd49f86
Ragav <Ragavendran.s2@cognizant.com>
2018-03-13 14:01:34 -0400

    first commit

---
+++ src/main/java/com/cognizant/codeblue/Immersion/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.cognizant.codeblue.Immersion.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/")
+public class WelcomeController {
+	
+	@RequestMapping(value = "/api/welcome", method = RequestMethod.GET)
+	public String welcomeToPALImmersion(){
+		return "Welcome to CodeBlue Immersion Lab !!";
+	}
+}
