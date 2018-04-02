3b338abb8fe9a48205e53017b216f10dc791ed6f
onedigitaladmin <33752008+onedigitaladmin@users.noreply.github.com>
2018-04-03 00:03:32 +0530

    Config Updated
    Config Updated

--- ci/scripts/test.sh
+++ ci/scripts/test.sh
@@ -3,7 +3,7 @@ set -ex
 export TERM=${TERM:-dumb}
 cd sampleproduct-1-services
 chmod 755 gradlew
-./gradlew --no-daemon build test jacocoTestReport
+./gradlew --no-daemon clean build jacocoTestReport
 cd build/reports/coverage
 apt-get update
 apt-get install -y zip
